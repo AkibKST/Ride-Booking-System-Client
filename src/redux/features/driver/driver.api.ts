832899029f6395b379c120ff4e4e3d51f7ec7ce8
AkibKST <akibkst22@gmail.com>
2025-11-26 21:32:44 +0600

    added driver api

--- src/redux/features/driver/driver.api.ts
+++ src/redux/features/driver/driver.api.ts
@@ -2,12 +2,15 @@ import { baseApi } from "@/redux/baseApi";
 
 export const driverApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
+    // Admin approve/suspend driver
     approvedSuspendToggle: builder.mutation({
       query: ({ id }) => ({
         url: `/driver/admin-approve/${id}`,
         method: "PATCH",
       }),
     }),
+
+    // Get all drivers
     getDrivers: builder.query({
       query: () => ({
         url: "/driver",
